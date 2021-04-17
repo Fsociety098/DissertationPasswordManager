d47e428df6e8ad8ec8d630759172ec0df1a433c1
harryalderton1 <57259095+harryalderton1@users.noreply.github.com>
2021-04-17 17:56:32 +0100

    added test for index

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -5,6 +5,7 @@ def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
-# def test_hello(client):
-#   response = client.get('/hello')
-#  assert response.data == b'Hello, World!'
+
+def test_index(client):
+    response = client.get('/')
+    assert b'<h1>Example headline.</h1>' in response.data
