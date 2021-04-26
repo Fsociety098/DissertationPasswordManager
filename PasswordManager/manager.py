0d9888005578a0e2f10b664a89250b4377a580d9
harryalderton1 <57259095+harryalderton1@users.noreply.github.com>
2021-04-26 22:26:39 +0100

    added filter criteria and endpoints

--- PasswordManager/manager.py
+++ PasswordManager/manager.py
@@ -1,9 +1,6 @@
-import functools
-import re
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, render_template, session
 )
-from werkzeug.security import check_password_hash, generate_password_hash
 
 from PasswordManager.db import get_db
 
@@ -18,4 +15,48 @@ def index():
     passwords = db.execute(
         'SELECT p.id, info.id, info.titlename, info.username FROM password p JOIN passwordinfo info '
         'on info.id = p.passwordinfoID WHERE p.userID = ?', (user_id,))
+    return render_template('manager/selectpassword.html', passwords=passwords)
+
+
+@bp.route('/sort/asc', methods=('GET', 'POST'))
+def asc():
+    db = get_db()
+    user_id = session.get('user_id')
+
+    passwords = db.execute(
+        'SELECT p.id, info.id, info.titlename, info.username FROM password p JOIN passwordinfo info '
+        'on info.id = p.passwordinfoID WHERE p.userID = ? ORDER BY titlename ASC', (user_id,))
+    return render_template('manager/selectpassword.html', passwords=passwords)
+
+
+@bp.route('/sort/desc', methods=('GET', 'POST'))
+def desc():
+    db = get_db()
+    user_id = session.get('user_id')
+
+    passwords = db.execute(
+        'SELECT p.id, info.id, info.titlename, info.username FROM password p JOIN passwordinfo info '
+        'on info.id = p.passwordinfoID WHERE p.userID = ? ORDER BY titlename DESC', (user_id,))
+    return render_template('manager/selectpassword.html', passwords=passwords)
+
+
+@bp.route('/sort/last-modified', methods=('GET', 'POST'))
+def lastmodified():
+    db = get_db()
+    user_id = session.get('user_id')
+
+    passwords = db.execute(
+        'SELECT p.id, info.id, info.titlename, info.username, info.lastmodified FROM password p JOIN passwordinfo info '
+        'on info.id = p.passwordinfoID WHERE p.userID = ? ORDER BY lastmodified desc', (user_id,))
+    return render_template('manager/selectpassword.html', passwords=passwords)
+
+
+@bp.route('/sort/last-created', methods=('GET', 'POST'))
+def lastcreated():
+    db = get_db()
+    user_id = session.get('user_id')
+
+    passwords = db.execute(
+        'SELECT p.id, info.id, info.titlename, info.username, info.lastmodified FROM password p JOIN passwordinfo info '
+        'on info.id = p.passwordinfoID WHERE p.userID = ? ORDER BY lastmodified desc ', (user_id,))
     return render_template('manager/selectpassword.html', passwords=passwords)
